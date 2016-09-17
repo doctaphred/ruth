4c7b40457224ecfa17a4bc0c397a865651cc6d87
Frederick Wagner <doctaphred@gmail.com>
2016-09-17 14:31:46 -0400

    Fix up ruby script

--- render_liquid.rb
+++ render_liquid.rb
@@ -3,15 +3,15 @@ require 'json'
 require 'liquid'
 
 
-def render_liquid
-    template = Liquid::Template.parse(ARGV[0], :error_mode => :strict)
-    context = JSON.parse(ARGV[1])
-    return template.render(JSON.parse(ARGV[1]), { strict_variables: true, strict_filters: true })
+def render_liquid(template_string, context_string)
+    template = Liquid::Template.parse(template_string, :error_mode => :strict)
+    context = JSON.parse(context_string)
+    return template.render(context, { strict_variables: true, strict_filters: true })
 end
 
 
 if __FILE__ == $0
-    print render_liquid()
+    print render_liquid(ARGV[0], ARGV[1])
 end
 
 
